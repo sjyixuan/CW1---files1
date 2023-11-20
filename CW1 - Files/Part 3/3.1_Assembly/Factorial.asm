c004275e73aaee03933a2f595f3d850f4f7687fe
davidjt01 <149792152+davidjt01@users.noreply.github.com>
2023-11-20 18:35:27 +0800

    Add files via upload

---
+++ CW1 - Files/Part 3/3.1_Assembly/Factorial.asm
@@ -0,0 +1,15 @@
+
+// ================================= Factorial ==================================
+// This program calculates the Factorial F of a given number n 
+// 	At run time:
+//		- The user should enter the value of n into R0. i.e. RAM[0]
+//		- The program then calculate the factorial of n    i.e. F(n)=n!
+//			 F(n) = n*(n-1)*(n-2)*......*2*1
+//		- The result F(n) should be saved in RAM[1]
+//			-- You should also consider the F(0) case.
+// ==============================================================================
+
+
+// put your code here
+
+
---
+++ CW1 - Files/Part 3/3.1_Assembly/Mult.asm
@@ -0,0 +1,10 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Mult.asm
+
+// Multiplies R1 and R2 and stores the result in R3.
+// (R1, R2, R3 refer to RAM[1], RAM[2], and RAM[3], respectively.)
+// The algorithm is based on repetitive addition.
+
+// put your code here
