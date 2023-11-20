42a3164f2ef273c587e35d0b3373f3e966a0c023
davidjt01 <149792152+davidjt01@users.noreply.github.com>
2023-11-20 19:33:07 +0800

    Update Mult.asm

--- CW1 - Files/Part 3/3.1_Assembly/Mult.asm
+++ CW1 - Files/Part 3/3.1_Assembly/Mult.asm
@@ -7,4 +7,34 @@
 // (R1, R2, R3 refer to RAM[1], RAM[2], and RAM[3], respectively.)
 // The algorithm is based on repetitive addition.
 
-// put your code here
+// put your code here 
+
+
+// Initialize R3 to 0
+@R3
+M=0
+
+// Loop to perform repetitive addition
+(LOOP)
+   // Check if R2 (counter) is 0
+   @R2
+   D=M
+   @END
+   D;JEQ
+
+   // Add R1 to R3
+   @R1
+   D=M
+   @R3
+   M=D+M
+
+   // Decrement R2 (counter)
+   @R2
+   M=M-1
+
+   // Jump back to the beginning of the loop
+   @LOOP
+   0;JMP
+
+// End of the program
+(END)
